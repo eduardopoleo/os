143903d134be772290d86c3b81e254c54b315945
Eduardo Poleo <eduardopoleo@gmail.com>
2018-06-19 22:25:37 -0400

    Re implemented with minimun help

--- readers_writers.c
+++ readers_writers.c
@@ -1,9 +1,14 @@
+// #include "./priority-readers-and-writers/priority-readers-and-writers.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define NUMBER_OF_READERS 5
 #define NUMBER_OF_WRITERS 5
+
+#define NUMBER_OF_READS 10
+#define NUMBER_OF_WRITES 3
 	
 pthread_mutex_t gShareMemoryLock = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t gReadPhase = PTHREAD_COND_INITIALIZER;
@@ -13,54 +18,62 @@ int gwaitingReaders = 0;
 int gReadersNum = 0;
 int gshareValue = 0;
 
-void *read(void *threadArgs) {
+void *readMain(void *threadArgs) {
 	int id = *((int*)threadArgs);
 
-	pthread_mutex_lock(&gShareMemoryLock);
-		gwaitingReaders++;
-		while(gReadersNum == -1) {
-			pthread_cond_wait(&gReadPhase, &gShareMemoryLock);
-		}
-		gwaitingReaders--;
-		gReadersNum++;
-	pthread_mutex_unlock(&gShareMemoryLock);
+	for (int i = 0; i < NUMBER_OF_READS; i++) {
+		usleep(1000 * (rand() % NUMBER_OF_READERS + NUMBER_OF_WRITERS));
 
-	printf("READING: id: %d, share value: %d, gReadersNum: %d\n", id, gshareValue, gReadersNum);
+		pthread_mutex_lock(&gShareMemoryLock);
+			gwaitingReaders++;
+			while(gReadersNum == -1) {
+				pthread_cond_wait(&gReadPhase, &gShareMemoryLock);
+			}
+			gwaitingReaders--;
+			gReadersNum++;
+		pthread_mutex_unlock(&gShareMemoryLock);
 
-	pthread_mutex_lock(&gShareMemoryLock);
-		gReadersNum--;
-		// can't these guys also wake up the readers. Does it make sense?
-		// This seems to prioritize the writer over the readers
-		if(gReadersNum == 0)	{
-			pthread_cond_signal(&gWritePhase);
-		}
-	pthread_mutex_unlock(&gShareMemoryLock);
+		printf("READING: id: %d, share value: %d, gReadersNum: %d\n", id, gshareValue, gReadersNum);
+
+		pthread_mutex_lock(&gShareMemoryLock);
+			gReadersNum--;
+			// can't these guys also wake up the readers. Does it make sense?
+			// This seems to prioritize the writer over the readers
+			if(gReadersNum == 0)	{
+				pthread_cond_signal(&gWritePhase);
+			}
+		pthread_mutex_unlock(&gShareMemoryLock);
+	}
 
 	pthread_exit(0);
 }
 
-void *write(void *threadArgs) {
+void *writeMain(void *threadArgs) {
 	int id = *(int*)threadArgs;
 
-	pthread_mutex_lock(&gShareMemoryLock);
-		while(gReadersNum != 0) {
-			pthread_cond_wait(&gWritePhase, &gShareMemoryLock);
-		}
-		gReadersNum = -1;
-	pthread_mutex_unlock(&gShareMemoryLock);
+	for (int i = 0; i < NUMBER_OF_WRITES; i++) {
+		usleep(1000 * (rand() % NUMBER_OF_READERS + NUMBER_OF_WRITERS));
 
-	gshareValue++;
+		pthread_mutex_lock(&gShareMemoryLock);
+			while(gReadersNum != 0) {
+				pthread_cond_wait(&gWritePhase, &gShareMemoryLock);
+			}
+			gReadersNum = -1;
+		pthread_mutex_unlock(&gShareMemoryLock);
 
-	printf("WRITING: id: %d, share value %d, gReadersNum %d\n:", id, gshareValue, gReadersNum);
+		gshareValue++;
 
-	pthread_mutex_lock(&gShareMemoryLock);
-		gReadersNum = 0;
-		if(gwaitingReaders > 0) {
-			pthread_cond_broadcast(&gReadPhase);
-		} else {
-			pthread_cond_signal(&gWritePhase);
-		}
-	pthread_mutex_unlock(&gShareMemoryLock);
+		printf("WRITING: id: %d, share value %d, gReadersNum %d:\n", id, gshareValue, gReadersNum);
+
+		pthread_mutex_lock(&gShareMemoryLock);
+			gReadersNum = 0;
+			if(gwaitingReaders > 0) {
+				pthread_cond_broadcast(&gReadPhase);
+			} else {
+				pthread_cond_signal(&gWritePhase);
+			}
+		pthread_mutex_unlock(&gShareMemoryLock);
+	}
 
 	pthread_exit(0);
 }
@@ -79,7 +92,7 @@ int main(int argc, char **argv) {
 		// address of the input to the read function 
 		// (address requires to be somewhere)
 		readerNum[i] = i;
-		pthread_create(&readers[i], NULL, read, &readerNum[i]);
+		pthread_create(&readers[i], NULL, readMain, &readerNum[i]);
 	}
 
 	for(int i = 0; i < NUMBER_OF_WRITERS; i++) {
@@ -91,7 +104,7 @@ int main(int argc, char **argv) {
 		// disasemble.
 
 		writerNum[i] = i;
-		pthread_create(&writers[i], NULL, write, &writerNum[i]);
+		pthread_create(&writers[i], NULL, writeMain, &writerNum[i]);
 	}
 
 	for(int i = 0; i < NUMBER_OF_READERS; i++) {
